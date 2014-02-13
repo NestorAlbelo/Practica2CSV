0a3d575c9e25a0a924e8db995c309c64952d61c5
Nestor Albelo Jorge <alu0100598322@ull.edu.es>
2014-02-13 16:39:23 +0000

    Añadimos style.css y csv.js

---
+++ csv.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+	$("#resultado").animate({opacity:0}, 0);
+   $("button").click(function() {
+     csv();
+   });
+ });
+
+
+function csv() {
+   var commonLength = NaN;
+	var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
+	var lines = document.getElementById("entrada").value.split(/\n+\s*/);
+	var r = [];
+	var row = "<% _.each(items, function(name) { %>"     +
+            "                    <td><%= name %></td>" +
+            "              <% }); %>";
+	
+	if (window.localStorage) localStorage.original  = document.getElementById("entrada").value;
+	
+	for(var t in lines) {
+		var m = lines[t].match(regexp);
+		var result = [];
+    	var error = false;
+		if (m) {
+			if (commonLength && (commonLength != m.length)) { 	//Se comprueba si la linea actual tiene el mismo tamaño que el tamaño por defecto
+        		error = true;
+        	}
+			else {														  	//Si no se iguala el tamaño por defecto al tamaño de la primera linea, ya que commonLength sera cero
+       		commonLength = m.length;
+        		error = false;
+      	}
+      	for(var i in m) {											  	//Limpiamos la cadena de espacios, comillas y comas
+        		result.push(m[i].replace(/,\s*$/,'').replace(/^\s*"/,'').replace(/"\s*$/,'').replace(/\\"/,'"'));
+      	}
+      	var tr = error? '<tr class="error">' : '<tr>';			
+      	r.push(tr+_.template(row, {items : result})+"</tr>");	//Introducimos la cadena en el array con una etiqueta de columna	
+      }
+      else {
+      	alert('Por favor revise la linea '+ lines[t] +' de su texto, ya que no se puede evaluar');
+      	error = true;
+    	}
+	}
+	if (!error) {
+		r.unshift('<p>\n<table class="center" id="result">');		//Introducimos al principio del array la cadena indicada
+  		r.push('</table>');													//Añadimos por la cola del array la cadena indicada
+  		resultado.innerHTML = r.join('\n');							//Juntamos en una sola cadena los elementos del array separados por el salto de linea	
+  		$("#resultado").animate({opacity:1}, 1500);
+  	}
+}
+
+
+window.onload = function() {
+  // If the browser supports localStorage and we have some stored data
+  if (window.localStorage && localStorage.original) {
+  		document.getElementById("entrada").value = localStorage.original;
+  }
+  else{
+	  	document.getElementById("entrada").placeholder = ("");
+  } 
+  	
+};
